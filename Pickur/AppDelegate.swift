02fb4fe3aff62b0d644d2625f602c7609dd5ac6c
LukaKerr <lukakerr1@gmail.com>
2017-10-04 10:24:41 +1100

    Indentation

--- Pickur/AppDelegate.swift
+++ Pickur/AppDelegate.swift
@@ -10,38 +10,38 @@ import Cocoa
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
-	
-	@IBOutlet weak var window: NSWindow!
-	
-	let popover = NSPopover()
-	let statusItem = NSStatusBar.system().statusItem(withLength: NSVariableStatusItemLength)
-	
-	func showPopover(sender: AnyObject?) {
-		if let button = statusItem.button {
-			popover.show(relativeTo: button.bounds, of: button, preferredEdge: NSRectEdge.minY)
-		}
-	}
- 
-	func closePopover(sender: AnyObject?) {
-		popover.performClose(sender)
-	}
- 
-	func togglePopover(sender: AnyObject?) {
-		if popover.isShown {
-			closePopover(sender: sender)
-		} else {
-			showPopover(sender: sender)
-		}
-	}
-	
-	func applicationDidFinishLaunching(_ notification: Notification) {
-		if let button = statusItem.button {
-			button.image = NSImage(named: "icon")
-			button.target = self
-			button.action = #selector(self.togglePopover(sender:))
-		}
-		popover.contentViewController = MenuViewController(nibName: "MenuViewController", bundle: nil)
-	}
-	
-	
+  
+  @IBOutlet weak var window: NSWindow!
+  
+  let popover = NSPopover()
+  let statusItem = NSStatusBar.system().statusItem(withLength: NSVariableStatusItemLength)
+  
+  func showPopover(sender: AnyObject?) {
+    if let button = statusItem.button {
+      popover.show(relativeTo: button.bounds, of: button, preferredEdge: NSRectEdge.minY)
+    }
+  }
+  
+  func closePopover(sender: AnyObject?) {
+    popover.performClose(sender)
+  }
+  
+  func togglePopover(sender: AnyObject?) {
+    if popover.isShown {
+      closePopover(sender: sender)
+    } else {
+      showPopover(sender: sender)
+    }
+  }
+  
+  func applicationDidFinishLaunching(_ notification: Notification) {
+    if let button = statusItem.button {
+      button.image = NSImage(named: "icon")
+      button.target = self
+      button.action = #selector(self.togglePopover(sender:))
+    }
+    popover.contentViewController = MenuViewController(nibName: "MenuViewController", bundle: nil)
+  }
+  
+  
 }
